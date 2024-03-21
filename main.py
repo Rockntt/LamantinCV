8ea27ca82c2d3ef338564867f9ce29e5d9892901
Rockntt <klochkovmv2007@gmail.com>
2024-03-21 22:51:57 +0300

    new arm lang files, output optimisation attempt

--- main.py
+++ main.py
@@ -8,18 +8,29 @@ pytesseract.pytesseract.tesseract_cmd = 'Tesseract/tesseract.exe'
 alphabet1 = "ԽՁՒՅԷՐՏԵԸԻՈՕՊՌՉԺՋՓԹԼԿՃՀՔՖԴՍԱԶՑԳՎԲՆՄՇՂԾ"
 alphabet2 = "ԱԲՑԷՌԵԽՈՒԹԵՅՈՒՄԵՆԻՔՈՒՄեՆԻՅոՒՅուՆԻքՈՒՅոՂՈՒԹՅՈւՆԻ"
 
+detection_shapes = []
+
 while True:
     ret, frame = cap.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    text_data = pytesseract.image_to_data(gray, output_type=pytesseract.Output.DICT, lang='hye', config='--psm 6')
+    text_data = pytesseract.image_to_data(gray, output_type=pytesseract.Output.DICT, lang='arm', config='--psm 6')
+
+    max_area = 0
+    max_rect = None
 
     for i in range(len(text_data['text'])):
-        if text_data['text'][i] and any(
-                char in text_data['text'][i] for char in alphabet1):
+        if text_data['text'][i] and any(char in text_data['text'][i] for char in alphabet2):
             (x, y, w, h) = (text_data['left'][i], text_data['top'][i], text_data['width'][i], text_data['height'][i])
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            area = w * h
+            if area > max_area:
+                max_area = area
+                max_rect = (x, y, w, h)
+
+    if max_rect is not None:
+        (x, y, w, h) = max_rect
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     cv2.imshow('Video', frame)
 
