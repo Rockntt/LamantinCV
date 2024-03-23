5473f7bf6f533d1e57c5079b04abe54ee8ee4ea7
Rockntt <klochkovmv2007@gmail.com>
2024-03-23 22:05:56 +0300

    code refactoring

--- main.py
+++ main.py
@@ -1,55 +1,81 @@
+# pylint: disable=no-name-in-module
+# pylint: disable=no-member
+
+"""
+Подключение необходимых библиотек (pip install -r requirements.txt)
+"""
 import cv2
 import numpy as np
 import pytesseract
 from PIL import ImageFont, ImageDraw, Image
 
+# Инициализация камеры
 cap = cv2.VideoCapture(0)
 
+# Инициализация бинарника установленного Tesseract-OCR
 pytesseract.pytesseract.tesseract_cmd = 'Tesseract-OCR/tesseract.exe'
 
-alphabet1 = "ԽՁՒՅԷՐՏԵԸԻՈՕՊՌՉԺՋՓԹԼԿՃՀՔՖԴՍԱԶՑԳՎԲՆՄՇՂԾ"
-alphabet2 = "ԱԲՑԷՌԵԽՈՒԹԵՅՈՒՄԵՆԻՔՈՒՄեՆԻՅոՒՅուՆԻքՈՒՅոՂՈՒԹՅՈւՆԻ"
+# Список букв армянского алфавита для сравнения вывода
+ALPHABET = "ԱԲԳԴԵԶԷԸԹԺԻԼԽԾԿՀՁՂՃՄՅՆՇՈՉՊՋՌՍՎՏՐՑՒՓՔ"
 
-fontpath = "FreeSerif.ttf"
-
-counter = 0
+# Инициализация шрифта, поддерживающего Unicode (для вывода армянских букв)
+UNICODE_FONT = ImageFont.truetype("FreeSerif.ttf", 130)
 
 
 def frame_preprocessing(f):
+    """
+    Функция frame_preprocessing обрабатывает изображение, делая его более пригодным для OCR
+    Параметры:
+        f: Изображение, которое нужно обработать
+    Возвращает:
+        inverted_f: Обработанное изображение
+    """
     f = cv2.GaussianBlur(f, (5, 5), 0)
-    gray = cv2.cvtColor(f, cv2.COLOR_BGR2GRAY)
-    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-    invert = 255 - thresh
-    return invert
+    gray_f = cv2.cvtColor(f, cv2.COLOR_BGR2GRAY)
+    thresh_f = cv2.threshold(gray_f, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    inverted_f = 255 - thresh_f
+    return inverted_f
 
 
+# Цикл обработки кадров
 while True:
-    ret, frame = cap.read()
+    ret, frame = cap.read()  # Чтение кадра из потока камеры
 
-    # img_h, img_w, ch = frame.shape
+    frame_p = frame_preprocessing(frame)  # Обработка полученного кадра
 
-    frame_p = frame_preprocessing(frame)
-
-    text_data = pytesseract.image_to_data(frame_p, output_type=pytesseract.Output.DICT, lang='arm+hye', config='--psm 10')
+    # Распознавание текста с помощью библиотеки Tesseract
+    text_data = pytesseract.image_to_data(
+                                        frame_p,
+                                        output_type=pytesseract.Output.DICT,
+                                        lang='arm+hye',  # Базовый язык + Дообучение
+                                        config='--psm 10')  # Ищет 1 символ
 
     for i in range(len(text_data['text'])):
-        if text_data['text'][i] and any(char in text_data['text'][i] for char in alphabet2):
-            (x, y, w, h) = (text_data['left'][i], text_data['top'][i], text_data['width'][i], text_data['height'][i])
-            if len(text_data['text'][i]) == 1 and text_data['text'][i] in alphabet2:
-                print(text_data['text'][i], w, h)
-                # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 4)
+        if len(text_data['text'][i]) == 1 and text_data['text'][i] in ALPHABET:
+            (x, y, w, h) = (text_data['left'][i],
+                            text_data['top'][i],
+                            text_data['width'][i],
+                            text_data['height'][i])
+            # print(text_data['text'][i], w, h) - Можно использовать для отладки
 
-                font = ImageFont.truetype(fontpath, 130)
-                img_pil = Image.fromarray(frame)
-                draw = ImageDraw.Draw(img_pil)
-                draw.text((x, y), text_data['text'][i], font=font, fill=(255,255,0,0))
-                frame = np.array(img_pil)
+            # Отрисовка сектора с найденным текстом. Отключено, поскольку бесполезно для задачи,
+            # поскольку Tesseract часто возвращает сектор по площади больший, чем сама буква в кадре
+            # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 4)
 
+            # Отрисовка распознанной буквы в координатах, переданных из image_to_data()
+            img_pil = Image.fromarray(frame)
+            draw = ImageDraw.Draw(img_pil)
+            draw.text((x, y), text_data['text'][i], font=UNICODE_FONT, fill=(255, 255, 0, 0))
+            frame = np.array(img_pil)
+
+    # Отображаются 2 потока: 1) с распознаванием; 2) без, но обработанный
     cv2.imshow('Video', frame)
     cv2.imshow('Preprocessed Video', frame_p)
 
+    # Завершение работы при нажатии клавиши "q"
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+# Завершение работы
 cap.release()
 cv2.destroyAllWindows()
