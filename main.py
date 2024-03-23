f09b9a0d06cdea2efa5e76a0cac615824890fd5e
Rockntt <klochkovmv2007@gmail.com>
2024-03-23 18:00:15 +0300

    little improvements + detection visualisation

--- main.py
+++ main.py
@@ -1,38 +1,49 @@
 import cv2
+import numpy as np
 import pytesseract
+from PIL import ImageFont, ImageDraw, Image
 
 cap = cv2.VideoCapture(0)
 
-pytesseract.pytesseract.tesseract_cmd = 'Tesseract/tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = 'Tesseract-OCR/tesseract.exe'
 
 alphabet1 = "ԽՁՒՅԷՐՏԵԸԻՈՕՊՌՉԺՋՓԹԼԿՃՀՔՖԴՍԱԶՑԳՎԲՆՄՇՂԾ"
 alphabet2 = "ԱԲՑԷՌԵԽՈՒԹԵՅՈՒՄԵՆԻՔՈՒՄեՆԻՅոՒՅուՆԻքՈՒՅոՂՈՒԹՅՈւՆԻ"
 
-detection_shapes = []
+fontpath = "FreeSerif.ttf"
+
+
+def frame_preprocessing(f):
+    gray = cv2.cvtColor(f, cv2.COLOR_BGR2GRAY)
+    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    invert = 255 - thresh
+    return invert
+
 
 while True:
     ret, frame = cap.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # img_h, img_w, ch = frame.shape
 
-    text_data = pytesseract.image_to_data(gray, output_type=pytesseract.Output.DICT, lang='arm', config='--psm 10')
+    frame_p = frame_preprocessing(frame)
 
-    max_area = 0
-    max_rect = None
+    text_data = pytesseract.image_to_data(frame_p, output_type=pytesseract.Output.DICT, lang='hye', config='--psm 10')
 
     for i in range(len(text_data['text'])):
         if text_data['text'][i] and any(char in text_data['text'][i] for char in alphabet2):
             (x, y, w, h) = (text_data['left'][i], text_data['top'][i], text_data['width'][i], text_data['height'][i])
-            area = w * h
-            if area > max_area:
-                max_area = area
-                max_rect = (x, y, w, h)
+            if len(text_data['text'][i]) == 1 and text_data['text'][i] in alphabet2:
+                print(text_data['text'][i], w, h)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 4)
 
-    if max_rect is not None:
-        (x, y, w, h) = max_rect
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                font = ImageFont.truetype(fontpath, 110)
+                img_pil = Image.fromarray(frame)
+                draw = ImageDraw.Draw(img_pil)
+                draw.text((50, 100), text_data['text'][i], font=font, fill=(0,0,0,0))
+                frame = np.array(img_pil)
 
     cv2.imshow('Video', frame)
+    cv2.imshow('Preprocessed Video', frame_p)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
